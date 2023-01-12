25180506f2212ea9f14c1b0eeafe9bc5999db168
Wang Dong <wzmhong@gmail.com>
2023-01-12 09:33:02 -0600

    Finish CountEqualOfPsTs and CountThe Vowels

--- CountEqualOfPsTs.js
+++ CountEqualOfPsTs.js
@@ -1,1 +1,14 @@
-to be finish.
+//The regex g means?
+
+function CountEqualOfPsTs(str)
+{
+    nPs = str.replace(/[^p]/g, '').length;
+    //console.log(str);
+    //nTs = str.replace(/[^t]/g, '').length;
+    console.log(str);
+    console.log(`Number of Ps is : ${nPs}, number of Ts is : ${nTs}.`);
+}
+
+CountEqualOfPsTs('test');
+CountEqualOfPsTs('function');
+CountEqualOfPsTs('ptptpt');
--- CountTheVowels.js
+++ CountTheVowels.js
@@ -1,1 +1,9 @@
-need to finish.
+function CheckNumberVowels(str)
+{
+    num = str.replace(/[^aeiou]/g, '').length;
+    return num;
+}
+
+console.log(CheckNumberVowels("action"));
+console.log(CheckNumberVowels("ivy"));
+console.log(CheckNumberVowels("what"));
